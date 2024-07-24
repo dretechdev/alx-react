da4a5c443e2632e9d65bd97e275149350575dae6
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2024-07-24 20:49:44 +0000

    Task 4

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
-export const getFullYear = function () {
-  return new Date().getFullYear();
-};
-
-export const getFooterCopy = function (isIndex) {
-  if (isIndex) return 'Holberton School';
-  return 'Holberton School main dashboard';
-};
