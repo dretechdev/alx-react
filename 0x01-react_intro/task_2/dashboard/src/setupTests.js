8077e143a11c9e752bad80d2a7dd8ae436729009
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2024-07-24 20:19:28 +0000

    Files

---
+++ 0x01-react_intro/task_2/dashboard/src/setupTests.js
@@ -0,0 +1,6 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
